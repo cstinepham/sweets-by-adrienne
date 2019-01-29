382463ceec531e5f329574a433b8cdd26d680e63
christine.pham <cstinepham@gmail.com>
2019-01-28 21:01:52 -0800

    Setup frontend

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
