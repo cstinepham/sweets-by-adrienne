f476382f2b718e4f052e89a49c80aafcc74b50fe
christine.pham <cstinepham@gmail.com>
2019-02-08 17:46:09 -0800

    Setup index.js and root cmponent

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// We will create this component shortly
 import Root from './components/root';
 
+// We set this up in the last section
+import configureStore from './store/store';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+      let store;
+
+      store = configureStore({});
+    
+      // Render our root component and pass in the store as a prop
+      const root = document.getElementById('root');
+
+      ReactDOM.render( < Root store = { store } />, root );
+      });
